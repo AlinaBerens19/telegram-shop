5e352a90c388d784536070dc98d94065fc874a5f
alina <berens.alina019@gmail.com>
2023-11-19 11:38:58 +0200

    Initial

---
+++ src/app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,8 @@
+
+const AuthLayout = ({ children } : {children: React.ReactNode}) => {
+  return <div className="bg-gray-200 h-full flex items-center justify-center">{children}</div>;
+}
+
+
+
+export default AuthLayout;
---
+++ src/hooks/useTelegram.tsx
@@ -0,0 +1,49 @@
+
+interface CustomWindow extends Window {
+    Telegram?: {
+        WebApp: {
+            close: () => void;
+            MainButton: {
+                isVisible: boolean;
+                hide: () => void;
+                show: () => void;
+            };
+            initDataUnsafe?: {
+                user?: {
+                    username?: string;
+                };
+                query_id?: string;
+            };
+        };
+    };
+}
+
+export function useTelegram() {
+    const tg = (window as CustomWindow).Telegram?.WebApp;
+
+    const onClose = () => {
+        if (tg) {
+            tg.close();
+        } else {
+            console.error("Telegram WebApp not available.");
+        }
+    };
+
+    const onToggleButton = () => {
+        if (tg && tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else if (tg) {
+            tg.MainButton.show();
+        } else {
+            console.error("Telegram WebApp not available.");
+        }
+    };
+
+    return {
+        tg,
+        onToggleButton,
+        onClose,
+        user: tg?.initDataUnsafe?.user?.username,
+        queryId: tg?.initDataUnsafe?.query_id,
+    };
+}
