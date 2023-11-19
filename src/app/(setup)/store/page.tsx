5e1136b9f677c91a2347856bd5bb56404f31ddf0
alina <berens.alina019@gmail.com>
2023-11-19 12:00:34 +0200

    Without next-theme

---
+++ src/app/(setup)/store/page.tsx
@@ -0,0 +1,11 @@
+
+
+const StorePage = () => {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      <p className="text-3xl text-bold">STORE</p>
+    </div>
+  )
+}
+
+export default StorePage
